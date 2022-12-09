4495ee58cca81811b6e6a75d63d417aee7e0298c
Kasper Alsén <alsenkasper@gmail.com>
2022-12-09 20:13:53 +0100

    menu started

--- Program.cs
+++ Program.cs
@@ -5,6 +5,34 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        bool running = true;
+        string? menuChoice = Console.ReadLine();
+        var ingredientsList = new List<string>();
+        while (running)
+        {   
+                    Console.Write("Username:\n" + Console.ReadLine());
+                    Console.Write("password:" + Console.ReadLine());
+
+            switch (menuChoice)
+            {
+            
+                case "1":
+                Console.Write("");
+                    break;
+
+                case "2":
+                    Console.WriteLine("1. Ange ingredienser:");
+                    break;
+
+                case "3":
+                    Console.WriteLine("1. Ange ingredienser:");
+                    break;
+
+                case "4":
+                    Console.WriteLine("1. Ange ingredienser:");
+                    break;
+
+            }
+        }
     }
 }
